ee49042f0ce31fd9dbdc2f923800214e4319be28
Amine EL G <aelghayate@simplon.co>
2016-10-07 14:17:35 +0200

    Update index-step4.js

--- index-step4.js
+++ index-step4.js
@@ -1,42 +1,29 @@
 $(document).ready(main);
-var nb_tentative;
-var nb_min;
-var nb_maxi;
-var nb_mystere;
+var nbTentative;
+var nbMystere;
 
 function main(){
-
-		
+	
 	$('#recupInput').click(function(){
-	nb_tentative = parseInt($('#tentatives').val(), 10);
-	console.log(nb_tentative);
-	nb_min = parseInt($('#mini').val(), 10);
-	console.log(nb_min);
-	nb_maxi = parseInt($('#maxi').val(), 10);
-	console.log(nb_maxi);
+		nbTentative = parseInt($('#tentatives').val(), 10);
+		var nb_min = parseInt($('#mini').val(), 10);
+		var nb_maxi = parseInt($('#maxi').val(), 10);
 
-	nb_mystere = parseInt(Math.random()*(nb_maxi - nb_min) + nb_min);
-	console.log(nb_mystere);
+		nbMystere = parseInt(Math.random()*(nb_maxi - nb_min) + nb_min);
 	});
 
-
 	function demarrerPartie(){
-		nb_tentative = parseInt($('#tentatives').val(), 10);
-		console.log(nb_tentative);
+		nbTentative = parseInt($('#tentatives').val(), 10);
 		nb_mystere = parseInt(Math.random()*(nb_maxi - nb_min) + nb_min);
-		console.log(nb_mystere);
 		$('span').text(nb_tentative);
-
-
 	}
 
 	function partieGagnee(){
 		demarrerPartie();
-
 	}
 
 	function partiePerdue(){
-		if(nb_tentative === 0){
+		if(nbTentative === 0){
 			alert('Perdu');
 			demarrerPartie();
 		}
@@ -47,28 +34,22 @@ function main(){
 	function clickValider(){
 		var contenu = parseInt($('#votre_nombre').val(), 10);
 
-		if(contenu === nb_mystere){
+		if(contenu === nbMystere){
 			alert('Gagné');
 			partieGagnee();
-		}
-
-		else if(contenu > nb_mystere){
+		} else if(contenu > nbMystere){
 			alert('Perdu, votre nombre est trop grand!');
-			nb_tentative --;
+			nbMystere --;
 			$('span').text(nb_tentative);
-		}
-
-		else if(contenu < nb_tentative){
+		} else if(contenu < nb_tentative){
 			alert('Perdu, votre nombre est trop petit')
-			nb_tentative --;
+			nbMystere --;
 			$('span').text(nb_tentative);
 		}
 		partiePerdue();
 	}
 	$('#go').click(function(){
 		clickValider();
-		console.log('coucou');
-
 	});
 
 }
